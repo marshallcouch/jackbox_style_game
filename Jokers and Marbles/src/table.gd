b0d4fbb7f068ce767480272d70552f908b44543d
Marshall Couch <marshall.couch@gmail.com>
2023-02-15 22:47:15 -0500

    changed some spacing

--- Jokers and Marbles/src/table.gd
+++ Jokers and Marbles/src/table.gd
@@ -11,17 +11,18 @@ func _ready() -> void:
 	_setup_deck()
 	_setup_start_menu()
 	print("done")
-	
 
 
 func _on_viewport_size_changed():
 	# Do whatever you need to do when the window changes!
 	_setup_start_menu()
 
+
 func _input(event) -> void:
 	if event.is_action_pressed("ui_menu"):
 		$Controls/StartMenu.show()
-			
+
+
 func _setup_start_menu():
 	var start_panel = $Controls/StartMenu/StartMenuPanel
 	start_panel.rect_size = get_viewport().size
@@ -34,10 +35,7 @@ func _setup_start_menu():
 	for button in vbox.get_children():
 		button.rect_min_size.y = 40
 
-	
 
-	
-#this _setup deck calls a function to create a normal deck of playing cards
 func _setup_deck() -> void:
 	decks.append(Utils.setup_standard_deck(true,true))
 
@@ -48,10 +46,11 @@ func _setup_players(num_players: int = 1) -> void:
 	for i in num_players:
 		empty_hand = []
 		player_hands.append(empty_hand)
-	
+
 
 func _place_card_in_players_hand(player, card) -> void:
 	player_hands[player].append(card)
+
 
 func draw_card(deck_to_draw_from: String = "") -> Dictionary:
 	if deck_to_draw_from != "":
@@ -61,17 +60,19 @@ func draw_card(deck_to_draw_from: String = "") -> Dictionary:
 					return deck["cards"].pop_front()
 	return decks[0]["cards"].pop_front()
 
+
 func discard(card_to_discard:Dictionary) -> void:
 	_discard_pile_list.add_item(card_to_discard["name"])
 	_discard_pile_list.move_item(discard_pile.size(),0)
 	discard_pile.push_front(card_to_discard)
 
+
 func play_card(card_to_play:Dictionary) -> void:
 	pass
-	
+
+
 func _on_DebugButton_pressed() -> void:
 	discard(draw_card())
-	
 
 
 func _on_start_menu_button_pressed(extra_arg_0: String) -> void:
