e631115a886f26253f5bf623b7b33d78d936af02
Marshall Couch <marshall.couch@gmail.com>
2023-02-15 22:43:17 -0500

    added a start menu

--- Jokers and Marbles/src/table.gd
+++ Jokers and Marbles/src/table.gd
@@ -9,8 +9,34 @@ onready var _discard_pile_list = $Cards/DiscardPile
 
 func _ready() -> void:
 	_setup_deck()
+	_setup_start_menu()
 	print("done")
+	
 
+
+func _on_viewport_size_changed():
+	# Do whatever you need to do when the window changes!
+	_setup_start_menu()
+
+func _input(event) -> void:
+	if event.is_action_pressed("ui_menu"):
+		$Controls/StartMenu.show()
+			
+func _setup_start_menu():
+	var start_panel = $Controls/StartMenu/StartMenuPanel
+	start_panel.rect_size = get_viewport().size
+	var vbox = $Controls/StartMenu/StartMenuPanel/StartMenuVbox
+	vbox.rect_size \
+		= Vector2(get_viewport().size.x *.8 \
+		,get_viewport().size.y  )
+	vbox.rect_position \
+		= Vector2(get_viewport().size.x *.1,0)
+	for button in vbox.get_children():
+		button.rect_min_size.y = 40
+
+	
+
+	
 #this _setup deck calls a function to create a normal deck of playing cards
 func _setup_deck() -> void:
 	decks.append(Utils.setup_standard_deck(true,true))
@@ -43,8 +69,19 @@ func discard(card_to_discard:Dictionary) -> void:
 func play_card(card_to_play:Dictionary) -> void:
 	pass
 	
+func _on_DebugButton_pressed() -> void:
+	discard(draw_card())
 	
 
 
-func _on_DebugButton_pressed() -> void:
-	discard(draw_card())
+func _on_start_menu_button_pressed(extra_arg_0: String) -> void:
+	if extra_arg_0 == "start_game":
+		pass
+	elif extra_arg_0 == "join_game":
+		pass
+	elif extra_arg_0 == "return_to_game":
+		$Controls/StartMenu.hide()
+		pass
+	elif extra_arg_0 == "quit":
+		get_tree().quit()
+		pass
