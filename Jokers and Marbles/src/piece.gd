70f4ca176cc29ca22933619a20f2d98209982c91
Marshall Couch <marshall.couch@gmail.com>
2023-02-26 13:59:55 -0500

    icons for the pieces and coloring of the base

--- Jokers and Marbles/src/piece.gd
+++ Jokers and Marbles/src/piece.gd
@@ -2,8 +2,10 @@ extends Node2D
 
 
 func _ready() -> void:
-	pass
+	set_base_color()
 
 func on_click():
 	print_debug("I've been clicked! piece")
 
+func set_base_color(color = Color(1,1,1,1)):
+	$PieceSprite.modulate = color
