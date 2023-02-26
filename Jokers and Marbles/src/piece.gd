7b5c09897f38e2974a5ba2c6826d9a33852f0ac6
Marshall Couch <marshall.couch@gmail.com>
2023-02-26 14:51:30 -0500

    bug fix for moving pieces around when zoomed in/out

--- Jokers and Marbles/src/piece.gd
+++ Jokers and Marbles/src/piece.gd
@@ -1,21 +1,37 @@
 extends Node2D
 
+const ICONS_IN_FOLDER = 20
+onready var base_sprite = $PieceSprite
+onready var icon_sprite = $PieceIconSprite
+onready var piece_base = $PieceBase
 
 func _ready() -> void:
 	set_base_color()
 	set_icon_color()
-	scale_piece(Vector2(.5,.5))
+	scale_piece(Vector2(.4,.4))
+	set_icon_image(2)
 
 func on_click():
 	print_debug("I've been clicked! piece")
 
 func set_base_color(color = Color(1,1,1,1)):
-	$PieceSprite.modulate = color
+	base_sprite.modulate = color
 
 func set_icon_color (color = Color(0,0,0,1)):
-	$PieceIconSprite.modulate = color
+	icon_sprite.modulate = color
 
 func scale_piece(new_scale:Vector2 = Vector2(1,1)):
-	$PieceBase.scale = new_scale
-	$PieceSprite.scale = new_scale
-	$PieceIconSprite.scale = new_scale
+	piece_base.scale = new_scale
+	base_sprite.scale = new_scale
+	icon_sprite.scale = new_scale
+
+func set_icon_image(index_of_image:int  = 1):
+	var dir = Directory.new()
+	dir.open("res://assets/sprites/piece_icon/")
+	if dir.file_exists(String(index_of_image) + ".png"):
+		var image = Image.new()
+		image.load("res://assets/sprites/piece_icon/" + String(index_of_image) + ".png")
+		var texture = ImageTexture.new()
+		texture.create_from_image(image)
+		icon_sprite.texture = texture
+	
