198a03fd85ffaca63f358a9b0a9cd16218e87c06
Marshall Couch <marshall.couch@gmail.com>
2023-02-25 22:11:33 -0500

    dragging works!

--- Jokers and Marbles/src/clicker.gd
+++ Jokers and Marbles/src/clicker.gd
@@ -3,18 +3,30 @@ extends Node2D
 
 var click_all = false
 var ignore_unclickable = true
-
+var previous_mouse_position = Vector2()
+var is_dragging:bool = false
+var dragging_shape
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1: # Left mouse click
 		var shapes = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position(), 32, [], 0x7FFFFFFF, true, true) # The last 'true' enables Area2D intersections, previous four values are all defaults
-
 		for shape in shapes:
 			if shape["collider"].has_method("on_click"):
 				shape["collider"].on_click()
-
+				dragging_shape = shape["collider"]
 				if !click_all and ignore_unclickable:
 					break # Thus clicks only the topmost clickable
-
 			if !click_all and !ignore_unclickable:
 				break # Thus stops on the first shape
+		previous_mouse_position = event.position
+		is_dragging = true
+
+	if is_dragging and event is InputEventMouseMotion:
+		var shapes = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position(), 32, [], 0x7FFFFFFF, true, true) # The last 'true' enables Area2D intersections, previous four values are all defaults
+		dragging_shape.position += (event.position - previous_mouse_position) # * get_tree().get_root().find_node("Camera2D").
+		previous_mouse_position = event.position
+	
+	if event is InputEventMouseButton and !event.pressed and event.button_index == 1: 
+		dragging_shape = null
+		is_dragging = false
+
--- Jokers and Marbles/src/piece.gd
+++ Jokers and Marbles/src/piece.gd
@@ -1,8 +1,10 @@
 extends Node2D
 
+var is_dragging:bool = false
 
 func _ready() -> void:
 	pass
 
 func on_click():
 	print_debug("I've been clicked! piece")
+	is_dragging = true
