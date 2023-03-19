8e51d5e3f0d9f9fd8c63fe79e72e20ca227415c5
Marshall Couch <marshall.couch@gmail.com>
2023-03-19 19:51:06 -0400

    cleaned up some stuff for http, connect from clientisn't working yet

--- Jokers and Marbles/src/networking.gd
+++ Jokers and Marbles/src/networking.gd
@@ -4,10 +4,12 @@ class_name Networking
 var is_server:bool = false
 var is_client:bool = false
 var is_connected:bool = false
-var peer = null
+var http_server:HTTPServer = null
+var peer:HTTPClient = null
 var player_list:Array = []
 const DEFAULT_PORT = 8282
 const DEFAULT_SERVER = "localhost"
+var HEADERS = [ "User-Agent: Pirulo/1.0 (Godot)", "Accept: */*"]
 
 func _ready() -> void:
 	print_debug("networking created")
@@ -15,70 +17,50 @@ func _ready() -> void:
 func start_server(port:int = DEFAULT_PORT):
 	print_debug("hosting_game on port " + str(port) + "...")
 	is_server = true
-	peer = HTTPServer.new()
-	peer.connect("client_connected", Callable(self, "_peer_connected"))
-	peer.connect("client_disconnected",  Callable(self, "_peer_disconnected"))
-	peer.connect("message_received",  Callable(self, "_on_data"))
-	var err = peer.listen(port)
+	http_server = HTTPServer.new()
+	var err = http_server.listen(port)
 	if err != OK:
 		print_debug("error starting server " + str(err))
 
 
 func join_game(server:String = DEFAULT_SERVER, port:int = DEFAULT_PORT):
-	var ws_url = "wss://" + server + ":" + str(port)
-	print_debug("joining game " + ws_url + "...")
+	var url = server + ":" + str(port)
 	is_client = true
 	peer = HTTPClient.new()
-	var err = peer.connect_to_url(ws_url)
+	var err = peer.connect_to_host(server, port)
 	if err != OK:
 		print_debug("Unable to connect " + str(err))
 	else:
 		print_debug("connecting...")
 		#peer.send()
+	OS.delay_msec(500)
+	peer.poll()
+	#peer.request(HTTPClient.METHOD_POST,"/index",HEADERS,"")
+	OS.delay_msec(500)
+	var hr = peer.has_response()
+	var bl = peer.get_response_body_length()
+	var code = peer.get_response_code()
+	var h = peer.get_response_headers_as_dictionary()
+	var rc = peer.is_response_chunked()
+	var response = peer.read_response_body_chunk()
+	
+	var req = HTTPRequest.new()
+
+	
+
+	
 
 
 func stop_game():
-	disconnect_game()
-	
-#both
-func disconnect_game(error = 0):
-	#server
-	if is_server:
-		peer.stop()
-		if peer.is_connected("client_connected", self, "_peer_connected"):
-			peer.disconnect("client_connected", self, "_peer_connected")
-		if peer.is_connected("client_disconnected", self, "_peer_disconnected"):
-			peer.disconnect("client_disconnected", self, "_peer_disconnected")
-		if peer.is_connected("client_close_request", self, "_peer_disconnected"):
-			peer.disconnect("client_close_request", self, "_peer_disconnected")
-		is_server = false
-
-	#client
 	if is_client:
-		if peer.is_connected("connection_closed", self, "_disconnect_game"):
-			peer.disconnect("connection_closed", self, "_disconnect_game")
-		if peer.is_connected("connection_error", self, "_disconnect_game"):
-			peer.disconnect("connection_error", self, "_disconnect_game")
-		if peer.is_connected("connection_established", self, "_server_connected"):
-			peer.disconnect("connection_established", self, "_server_connected")
+		peer.free()
+		peer = null
 		is_client = false
-	#both
-	if peer.is_connected("data_received", self, "_on_data"):
-		peer.disconnect("data_received", self, "_on_data")
-		
-	print_debug("Disconnected")
-
-
-#when the game client has connected to the server
-func _server_connected():
-	print_debug("This client is now connected to server")
-	send_packet("test packet from client")
-
-#server has peer connected
-func _peer_connected(id):
-	print_debug( get_time() + "player connected to server: " + str(id))
-	player_list.append({"player_id":id,"player_name":"default"})
-	send_packet("test packet from server",id)
+	
+	if is_server:
+		http_server.free()
+		http_server = null
+		is_server = false
 
 func get_time()-> String:
 	var t = Time.get_datetime_dict_from_system()
@@ -104,10 +86,11 @@ func send_packet(packet_content:String,peer_id:int = 1) -> void:
 
 
 func _process(_delta):
-	if is_client or is_server:
-		peer.poll()
+	if is_server:
+		http_server.poll()
 		
 	if is_client:
+		peer.poll()
 		var status = peer.get_ready_state()
 			
 
@@ -117,5 +100,36 @@ func _on_data(id:int = 0) -> void:
 	print_debug("Got data from client %d: %s " % [id, pkt])
 	
 	
+
+
+
+class HTTPServer:
+	var server: TCPServer
+	var clients: Array[StreamPeerTCP] = []
+
+	func listen(port:int) -> Error:
+		server = TCPServer.new()
+		var err = server.listen(port)
+		return err
+
+	func poll() -> void:
+		if server.is_connection_available():
+			var client = server.take_connection()
+			clients.append(client)
+			client.set_no_delay(true)
+			print("Client connected: ")
+			var response= "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nHello, world!"
+			client.put_data(response.to_ascii_buffer())
+
+		for client in clients:
+			if client.get_connected_host():
+				var data = client.get_available_bytes()
+				if data > 0:
+					var message = client.get_string(data)
+					print("Received message: ", message)
+
+		for client in clients:
+			if client.get_connected_host():
+				pass
 
 
